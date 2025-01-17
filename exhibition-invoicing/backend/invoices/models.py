e6cea6c55e2fbc2f5c1d385f0ba0cb962cdafb65
Rishab-Sudhir <rishabsudhir2002@gmail.com>
2025-01-17 18:34:20 +0400

    setting backend and frontend up
    alot happened just need to push to see whats new

--- exhibition-invoicing/backend/invoices/models.py
+++ exhibition-invoicing/backend/invoices/models.py
@@ -12,12 +12,12 @@ class Invoice(models.Model):
     def __str__(self):
         return f"Invoice {self.id} - {self.customer_name}"
     
-    class LineItem(models.Model):
-        invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE, related_name='line_items')
-        vendor_name = models.CharField(max_length=100)
-        description = models.CharField(max_length=255)
-        code = models.CharField(max_length=50, blank=True)
-        price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+class LineItem(models.Model):
+    invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE, related_name='line_items')
+    vendor_name = models.CharField(max_length=100)
+    description = models.CharField(max_length=255)
+    code = models.CharField(max_length=50, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
-        def __str__(self):
-            return f"{self.description} - {self.price}"
+    def __str__(self):
+        return f"{self.description} - {self.price}"
